832e9f19fea519e3d5da9057e0dd20183c823380
chadrien <hadrien.collongues@francepronet.com>
2013-04-05 11:04:55 +0200

    Correction ApiObject, changement méthode save

--- lib/Fpn/ApiClient/Core/ApiObject/ApiObjectInterface.php
+++ lib/Fpn/ApiClient/Core/ApiObject/ApiObjectInterface.php
@@ -6,6 +6,7 @@ interface ApiObjectInterface
 {
     public function fetch($id);
     public function fetchAll($page = 1, $limit = 20);
-    public function save($datas = array());
     public function delete();
+
+    protected function saveItem($datas = array());
 }
